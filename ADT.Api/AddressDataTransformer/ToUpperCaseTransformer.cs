a1b689df9d0fc12414ed0ffc4d7e2ebeb2c37bd3
Aram Zhamkochyan <aramzham@hotmail.com>
2023-01-27 12:49:42 +0100

    Delete ToUpperCaseTransformer.cs

--- ADT.Api/AddressDataTransformer/ToUpperCaseTransformer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ADT.Api.AddressDataTransformer;
-
-public class ToUpperCaseTransformer : IAddressDataTransformer
-{
-    public string Transform(string input)
-    {
-        return input.ToUpperInvariant();
-    }
-}
